415b01d204cebc364ce877c17a34061cd747cba9
derpthebass <ramboxp@live.com>
2013-10-21 02:27:01 -0400

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,54 @@
+var irc = require('irc');
+var io = require('socket.io').listen(1337);
+var channel = '#random';
+var autoOP = '';
+var name = 'IRC Bot';
+var network = 'irc.freenode.net';
+
+var bot = new irc.Client(network, name, { channels : [channel] });
+
+io.sockets.on('connection', function(socket) {
+        bot.addListener('join', function(channel, who) {
+            if (who !== name) socket.emit('joinFromIRC', who, channel);
+            if (who === autoOP) bot.send('MODE', channel, '+o', autoOP);
+        });
+        bot.addListener('quit', function(who, reason, channel, message) {
+            socket.emit('leaveFromIRC', who, channel);
+        });
+        bot.addListener('message', function(from, to, text, message) {
+            if (text.indexOf('!x') !== 0 && text.indexOf('!tellraw') !== 0) socket.emit('messageFromIRC', from, text);
+            else if (text.indexOf('!tellraw') === 0) socket.emit('tellRaw', text.substring(8));
+        });
+        bot.addListener('+mode', function(channel, by, mode, argument, message){
+        	socket.emit('+modeChange', by, mode, argument);
+        });
+        bot.addListener('-mode', function(channel, by, mode, argument, message){
+        	socket.emit('-modeChange', by, mode, argument);
+        });
+        bot.addListener('nick', function(oldNick, newNick) {
+            socket.emit('nickFromIRC', oldNick, newNick);
+        });
+        bot.addListener('kick', function(channel, who, by, reason, message){
+            socket.emit('kickFromIRC', who, channel, by);
+        });
+        bot.addListener('kill', function(who, reason, channel, message){
+            socket.emit('leaveFromIRC', who, channel);
+        });
+        bot.addListener('part', function(chanel, who, reason, message){
+            socket.emit('leaveFromIRC', who, channel);
+        });
+        socket.on('joinFromPlug', function(username) {
+            bot.say(channel, username + ' joined the room');
+        });
+        socket.on('leaveFromPlug', function(username) {
+            bot.say(channel, username + ' left the room');
+        });
+        socket.on('messageFromPlug', function(username, message, type){
+            if (type === 'message') bot.say(channel, username + ': ' + message);
+            else if (type === 'emote') bot.say(channel, '*' + username + message);
+        });
+});
+
+
+
+
