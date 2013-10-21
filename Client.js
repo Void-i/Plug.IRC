00b34dfc29a65054edc8cb73aeab244c3af017e4
derpthebass <ramboxp@live.com>
2013-10-21 17:51:03 -0400

    Added skip support

--- Client.js
+++ Client.js
@@ -1,6 +1,6 @@
 cleanString = function(string){return string.replace(/&#39;/g, "'").replace(/&amp;/g, "&").replace(/&#34;/g, "\"").replace(/&#59;/g, ";").replace(/&lt;/g, "<").replace(/&gt;/g, ">")}
 
-var socket = io.connect('http://playmc.pw:1337');
+var socket = io.connect('<socket server>');
 	socket.on('connect', function(){
 		socket.on('joinFromIRC', function(who, channel){
 			API.sendChat('/me ' + who + ' has joined ' + channel);
@@ -38,6 +38,9 @@ var socket = io.connect('http://playmc.pw:1337');
 		});
 		API.on(API.USER_LEAVE, function(user){
 			socket.emit('leaveFromPlug', user.username);
+		});
+		API.on(API.MOD_SKIP, function(username){
+			socket.emit('skipFromPlug', username);
 		});
 		API.on(API.CHAT, function(data){
 			if(data.fromID != API.getUser().id) socket.emit('messageFromPlug', data.from, cleanString(data.message), data.type);
