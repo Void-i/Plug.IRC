c4fe01514a6575d8bb7fb38ffd4eda3faca6923f
derpthebass <ramboxp@live.com>
2013-10-30 19:36:46 -0400

    Decided to remove a few things as they were more annoying than helpful

--- Client.js
+++ Client.js
@@ -8,9 +8,6 @@ var socket = io.connect('<socket server>');
 		socket.on('leaveFromIRC', function(who, channel){
 			API.sendChat('/me ' + who + ' has left ' + channel);
 		});
-		socket.on('tellRaw', function(message){
-			API.sendChat(cleanString(message));
-		})
 		socket.on('messageFromIRC', function(from, text){
 			API.sendChat(from + ': ' + text);
 		});
@@ -19,12 +16,6 @@ var socket = io.connect('<socket server>');
 		});
 		socket.on('kickFromIRC', function(nick, channel, by){
 			API.sendChat('/me ' + nick + ' was kicked from ' + channel + ' by ' + by);
-		});
-		socket.on('+modeChange', function(by, mode, argument){
-			API.sendChat('/me ' + by + ' set ' + argument + ' to +' + mode);
-		});
-		socket.on('-modeChange', function(by, mode, argument){
-			API.sendChat('/me ' + by + ' set ' + argument + ' to -' + mode);
 		});
 		socket.on('requestUsers', function(){
 			users = [];
@@ -38,9 +29,6 @@ var socket = io.connect('<socket server>');
 		});
 		API.on(API.USER_LEAVE, function(user){
 			socket.emit('leaveFromPlug', user.username);
-		});
-		API.on(API.MOD_SKIP, function(username){
-			socket.emit('skipFromPlug', username);
 		});
 		API.on(API.CHAT, function(data){
 			if(data.fromID != API.getUser().id) socket.emit('messageFromPlug', data.from, cleanString(data.message), data.type);
